33d141c093dfa29555802b8c1abd800ca8a0d996
Danda Prayogi Wijaya <dandaprayogiwijaya29@gmail.com>
2025-12-26 21:14:12 +0700

    Edit

--- src/main/java/com/praktikum/testing/automation/pages/ProductPage.java
+++ src/main/java/com/praktikum/testing/automation/pages/ProductPage.java
@@ -2,6 +2,7 @@ package com.praktikum.testing.automation.pages;
 
 import com.praktikum.testing.automation.base.BasePage;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ProductPage extends BasePage {
 
@@ -18,7 +19,9 @@ public class ProductPage extends BasePage {
 
     public void addToCart() {
         driver.findElement(addToCartBtn).click();
-        Alert alert = driver.switchTo().alert();
+
+        // ✅ WAIT alert dengan aman
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
         alert.accept();
     }
 }
